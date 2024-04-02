ab3fb8061f5caf6403b68b2fe32cca637da0b8aa
Ayanda Manzini <143455650+ReggieWatersin@users.noreply.github.com>
2024-04-02 08:53:05 +0000

    Add files via upload

---
+++ JavaApplication2.java
@@ -0,0 +1,34 @@
+
+package javaapplication2;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     int num1 = 10;
+     float num2 = 10;
+     double num3 = 10;
+     String name = "Ayanda";
+     Boolean check = true;
+     char c = 66;
+     //refence type = Array
+     int[] numbers = {1,2,3,4,5};
+     System.out.println(num1);
+     System.out.println(num2);
+     System.out.println(num3);
+     System.out.println(name);
+     System.out.println(check);
+     System.out.println(c);
+     System.out.println(Arrays.toString(numbers));
+    }
+    
+}
